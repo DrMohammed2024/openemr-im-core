07ddbb7b467ddc5f9475bc768f329cf962c9a6d5
DrMohammed2024 <drm.alqady@gmail.com>
2026-04-14 01:14:18 +0300

    Stabilize save.php as POST-only compatibility wrapper
    Keep save.php minimal and delegate POST handling to new.php without adding separate form logic.

--- interface/forms/internal_medicine/save.php
+++ interface/forms/internal_medicine/save.php
@@ -1,14 +1,8 @@
 <?php
 
-require_once("../../globals.php");
-
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    $target = "new.php";
-    if (isset($_GET['id']) && $_GET['id'] !== '') {
-        $target .= '?id=' . urlencode((string) $_GET['id']);
-    }
-    header("Location: " . $target);
+    header("Location: new.php");
     exit;
 }
 
-require_once(__DIR__ . "/new.php");
+require_once(__DIR__ . '/new.php');
